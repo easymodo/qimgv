f5709ace6d365c08395dee6620f3f3bc04b9e576
easymodo <easymodofrf@gmail.com>
2015-09-22 17:45:09 +0300

    add thumbnailer class
    Signed-off-by: easymodo <easymodofrf@gmail.com>

---
+++ thumbnailer.h
@@ -0,0 +1,25 @@
+#ifndef THUMBNAILER_H
+#define THUMBNAILER_H
+
+#include <QRunnable>
+#include <QThread>
+#include <sourceContainers/thumbnail.h>
+#include "imagecache.h"
+#include <imagefactory.h>
+
+class Thumbnailer : public QObject, public QRunnable
+{
+    Q_OBJECT
+public:
+    Thumbnailer(ImageCache* _cache, QString _path, int _target);
+
+    void run();
+    QString path;
+    int target;
+private:
+    ImageCache* cache;
+signals:
+    void thumbnailReady(int);
+};
+
+#endif // THUMBNAILER_H
