119deba0a44cbd30a06e82ecf82d29277708090a
sevoan <sevoan@gmail.com>
2015-09-25 14:51:24 +0300

    Improving code quality

--- settingsdialog.cpp
+++ settingsdialog.cpp
@@ -37,11 +37,7 @@ void SettingsDialog::readSettings() {
     // ##### scaling #####
 
     setting = globalSettings->useFastScale();
-    if(setting==true) {
-        ui->scalingQualityComboBox->setCurrentIndex(1);
-    } else {
-        ui->scalingQualityComboBox->setCurrentIndex(0);
-    }
+    ui->scalingQualityComboBox->setCurrentIndex(setting ? 1 : 0);
 
     // ##### fit mode #####
     int tmp = globalSettings->imageFitMode();
@@ -96,11 +92,9 @@ void SettingsDialog::applySettings() {
 
     globalSettings->setFfmpegExecutable(ui->ffmpegLineEdit->text());
 
-    if(ui->scalingQualityComboBox->currentIndex()==1) {
-        globalSettings->setUseFastScale(true);
-    } else {
-        globalSettings->setUseFastScale(false);
-    }
+    bool useFastScale = ui->scalingQualityComboBox->currentIndex() == 1;
+    globalSettings->setUseFastScale(useFastScale);
+
     globalSettings->setBackgroundColor(bgLabelPalette.color(QPalette::Window));
     globalSettings->setAccentColor(accentLabelPalette.color(QPalette::Window));
 
