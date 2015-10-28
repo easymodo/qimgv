1a6fd57b90e6a86a26fd69fa8f3ded8aa534051f
mitcher <mitcher@localhost.localdomain>
2015-10-28 15:44:41 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-rm -rf ./build
-mkdir build && cd build
-cmake ../ -DCMAKE_INSTALL_PREFIX=/usr/
-make -j3
-chmod +x ./qimgv
-cp ./qimgv ../qimgv
-echo "##################################################################";
-echo "NOTE:";
-echo "make sure to put path to ffmpeg/avconv executable in preferences";
-echo "/usr/bin/ffmpeg";
-echo "or";
-echo "/usr/bin/avconv";
-echo "In ubuntu avconv is provided by libav-tools package."
+#rm -rf ./build
+#mkdir build && cd build
 make clean
-cd ..
-
+sudo find . -name 'moc_*' -exec rm -rf {} \;
+cmake . -DCMAKE_INSTALL_PREFIX=/usr/
+make -j4
+#chmod +x ./qimgv
+#cp ./qimgv ../qimgv
+#echo "##################################################################";
+#echo "NOTE:";
+#echo "make sure to put path to ffmpeg/avconv executable in preferences";
+#echo "/usr/bin/ffmpeg";
+#echo "or";
+#echo "/usr/bin/avconv";
+#echo "In ubuntu avconv is provided by libav-tools package."
+#make clean
+#cd ..
+Now run install.sh as root
