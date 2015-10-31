7043cbea64dc7d07718bbac87999066bde566da7
dustysesameseed <dustysesameseed@gmail.com>
2015-10-31 09:08:21 -0400

    Add Clip, for real this time
    also fixes crash from cycling through videos too quickly.

---
+++ sourceContainers/clip.cpp
@@ -0,0 +1,65 @@
+#include "clip.h"
+
+Clip::Clip(const QString &fileName, const char* format) {
+    load(fileName, format);
+}
+
+Clip::~Clip() {
+
+}
+
+void Clip::load(const QString &fileName, const char* format) {
+    QString ffmpegExe = settings->ffmpegExecutable();
+    if(ffmpegExe.isEmpty()) {
+        return;
+    }
+
+    QString command = "\"" + ffmpegExe + "\"" + " -i " + "\"" + fileName + "\"";
+    QProcess process;
+    process.start(command);
+    process.waitForFinished(100);
+    QByteArray out = process.readAllStandardError();
+    process.close();
+
+    QRegExp expResolution("[0-9]+x[0-9]+");
+    QRegExp expWidth("[0-9]+\\B");
+    QRegExp expHeight("\\B+[0-9]+$");
+    expResolution.indexIn(out);
+    QString res = expResolution.cap();
+    expWidth.indexIn(res);
+    expHeight.indexIn(res);
+    QString wt = expWidth.cap();
+    QString ht = expHeight.cap();
+
+    path = fileName;
+    extension = format;
+    srcWidth = wt.toInt();
+    srcHeight = ht.toInt();
+    grad = 0;
+    frame = QRect(0,0,srcWidth, srcHeight);
+}
+
+void Clip::save(const QString &fileName, const char* format, int quality) {
+    Q_UNUSED(fileName)
+    Q_UNUSED(format)
+    Q_UNUSED(quality)
+
+    // TODO: Use ffmpeg functions clip and rotate to save video
+}
+
+int Clip::width() {
+    return frame.width();
+}
+
+int Clip::height() {
+    return frame.height();
+}
+
+QSize Clip::size() {
+    return QSize(frame.width(), frame.height());
+}
+
+void Clip::rotate(qreal angle) {
+    grad += angle;
+    transform.rotate(angle);
+}
---
+++ sourceContainers/clip.h
@@ -0,0 +1,42 @@
+#ifndef CLIP_H
+#define CLIP_H
+
+#include <QString>
+#include <QRect>
+#include <QTransform>
+#include <QProcess>
+#include "../settings.h"
+
+class Clip {
+public:
+    Clip(const QString &fileName, const char* format);
+    ~Clip();
+
+    // functions which mimic those found in other underlying
+    // image types such as QImage
+    void load(const QString &fileName, const char* format);
+    void save(const QString &fileName, const char* format, int quality);
+    int height();
+    int width();
+    QSize size();
+    void rotate(qreal angle);
+
+    // functions intended to be used by videoplayer
+    QString getPath();
+    QRect getFrame();
+    QTransform getTransform();
+
+private:
+    QString path;
+    const char* extension;
+    QRect frame;
+    QTransform transform;
+    int grad;
+    uint srcWidth;
+    uint srcHeight;
+
+};
+
+
+#endif // CLIP_H
+
