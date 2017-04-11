a49196fe800c520337170f2f82d3068b3d426313
easymodo <easymodofrf@gmail.com>
2017-04-11 12:09:05 +0300

    Version 0.49

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationName("greenpepper software");
     QCoreApplication::setOrganizationDomain("github.com/easymodo/qimgv");
     QCoreApplication::setApplicationName("qimgv");
-    QCoreApplication::setApplicationVersion("0.49dev");
+    QCoreApplication::setApplicationVersion("0.49");
 
 
     settings = Settings::getInstance();
