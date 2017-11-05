2967c65368740190069cb1481dd8f7d04612a6c4
easymodo <easymodofrf@gmail.com>
2017-11-06 01:51:25 +0200

    minor stuff

--- components/scaler/scalerrunnable.cpp
+++ components/scaler/scalerrunnable.cpp
@@ -11,9 +11,9 @@ void ScalerRunnable::run() {
     emit started(req);
     ImageLib imgLib;
     QImage *scaled = NULL;
-    if(req.size.width() > req.image->width() && !settings->smoothUpscaling())
+    if( req.size.width() > req.image->width() && !settings->smoothUpscaling() || settings->scalingFilter() == 0 )
         scaled = imgLib.scale(req.image->getImage(), req.size, 0);
-    else if((float)req.size.width()*req.size.height() / 1000000 > 23.0) // todo: test on different pc & tune value
+    else if( (float)req.size.width()*req.size.height() / 1000000 > 23.0 ) // todo: test on different pc & tune value
         scaled = imgLib.scale(req.image->getImage(), req.size, 1);
     else
         scaled = imgLib.scale(req.image->getImage(), req.size, settings->scalingFilter());
