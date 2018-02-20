0e6d588bd8c62e4c969efd3702228de6f8fa8c89
easymodo <easymodofrf@gmail.com>
2018-02-20 22:19:20 +0200

    v0.6.1

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationName("greenpepper software");
     QCoreApplication::setOrganizationDomain("github.com/easymodo/qimgv");
     QCoreApplication::setApplicationName("qimgv");
-    QCoreApplication::setApplicationVersion("0.6");
+    QCoreApplication::setApplicationVersion("0.6.1");
 
     // needed for mpv
     std::setlocale(LC_NUMERIC, "C");
