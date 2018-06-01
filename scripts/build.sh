a8c739a772d7a5e4793c2b9425b737b99f350042
easymodo <easymodofrf@gmail.com>
2018-06-01 09:02:50 +0300

    [CMake] Set VIDEO_SUPPORT=ON by default

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,10 +11,12 @@ cd build
 echo "Cleaning up build directory..."
 make clean
 
-# All options are off by default.
-# Currently available are: VIDEO_SUPPORT, KDE_BLUR
-# Example - blur support, no video support:
+# Available options: 
+# VIDEO_SUPPORT (default: ON)
+# KDE_BLUR      (default: OFF)
+# Example - build with blur & video support:
 # cmake -DKDE_BLUR=ON -DCMAKE_INSTALL_PREFIX=/usr/ -DCMAKE_BINARY_DIR=${DIR}/ ..
+
 cmake -DVIDEO_SUPPORT=ON -DCMAKE_INSTALL_PREFIX=/usr/ -DCMAKE_BINARY_DIR=${DIR}/ ..
 
 make -j4
