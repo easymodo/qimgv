fbdd24b9f252fa5223eb6e2fe2a4de9816c8e8eb
easymodo <easymodofrf@gmail.com>
2019-02-11 13:01:03 +0200

    [ContextMenu] Change WindowType

--- gui/contextmenu.cpp
+++ gui/contextmenu.cpp
@@ -6,8 +6,9 @@ ContextMenu::ContextMenu(QWidget *parent) :
     ui(new Ui::ContextMenu)
 {
     ui->setupUi(this);
-    setWindowFlags(Qt::Tool | Qt::FramelessWindowHint | Qt::WindowDoesNotAcceptFocus);
-    setAttribute(Qt::WA_X11NetWmWindowTypeMenu, true);
+    //setWindowFlags(Qt::Popup | Qt::FramelessWindowHint | Qt::WindowDoesNotAcceptFocus);
+    setWindowFlags(Qt::Popup);
+    //setAttribute(Qt::WA_X11NetWmWindowTypeMenu, true);
     setAttribute(Qt::WA_TranslucentBackground, true);
     setAttribute(Qt::WA_NoMousePropagation, true);
     this->hide();
