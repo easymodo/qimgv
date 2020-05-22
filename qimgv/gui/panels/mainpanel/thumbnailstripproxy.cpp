15eed3a8ea8477ff558ab1ba7ae836dd77641a94
easymodo <easymodofrf@gmail.com>
2020-05-22 12:12:07 +0300

    forgot some files

---
+++ qimgv/gui/panels/mainpanel/thumbnailstripproxy.cpp
@@ -0,0 +1,112 @@
+#include "thumbnailstripproxy.h"
+
+ThumbnailStripProxy::ThumbnailStripProxy(QWidget *parent)
+    : QWidget(parent)
+{
+    layout.setContentsMargins(0,0,0,0);
+}
+
+void ThumbnailStripProxy::init() {
+    if(thumbnailStrip)
+        return;
+    thumbnailStrip.reset(new ThumbnailStrip());
+    thumbnailStrip->setParent(this);
+    layout.addWidget(thumbnailStrip.get());
+    this->setFocusProxy(thumbnailStrip.get());
+    this->setLayout(&layout);
+
+    connect(thumbnailStrip.get(), &ThumbnailStrip::itemSelected, this, &ThumbnailStripProxy::itemSelected);
+    connect(thumbnailStrip.get(), &ThumbnailStrip::thumbnailsRequested, this, &ThumbnailStripProxy::thumbnailsRequested);
+
+    thumbnailStrip->show();
+
+    // apply buffer
+    thumbnailStrip->populate(stateBuf.itemCount);
+    thumbnailStrip->selectIndex(stateBuf.selectedIndex);
+    // wait till layout stuff happens
+    // before calling focusOn()
+    qApp->processEvents();
+    thumbnailStrip->focusOn(stateBuf.selectedIndex);
+}
+
+void ThumbnailStripProxy::populate(int count) {
+    if(thumbnailStrip) {
+        thumbnailStrip->populate(count);
+    } else {
+        stateBuf.itemCount = count;
+    }
+}
+
+void ThumbnailStripProxy::setThumbnail(int pos, std::shared_ptr<Thumbnail> thumb) {
+    if(thumbnailStrip) {
+        thumbnailStrip->setThumbnail(pos, thumb);
+    }
+}
+
+void ThumbnailStripProxy::selectIndex(int index) {
+    if(thumbnailStrip) {
+        thumbnailStrip->selectIndex(index);
+    } else {
+        stateBuf.selectedIndex = index;
+    }
+}
+
+int ThumbnailStripProxy::selectedIndex() {
+    if(thumbnailStrip) {
+        return thumbnailStrip->selectedIndex();
+    } else {
+        return stateBuf.selectedIndex;
+    }
+}
+
+void ThumbnailStripProxy::focusOn(int index) {
+    if(thumbnailStrip) {
+        thumbnailStrip->focusOn(index);
+    }
+}
+
+void ThumbnailStripProxy::insertItem(int index) {
+    if(thumbnailStrip) {
+        thumbnailStrip->insertItem(index);
+    } else {
+        stateBuf.itemCount++;
+    }
+}
+
+void ThumbnailStripProxy::removeItem(int index) {
+    if(thumbnailStrip) {
+        thumbnailStrip->removeItem(index);
+    } else {
+        stateBuf.itemCount--;
+        if(index < stateBuf.selectedIndex) {
+            stateBuf.selectedIndex--;
+        } else if(index == stateBuf.selectedIndex) {
+            if(stateBuf.selectedIndex >= stateBuf.itemCount)
+                stateBuf.selectedIndex = stateBuf.itemCount - 1;
+            else
+                stateBuf.selectedIndex = index;
+        }
+    }
+}
+
+void ThumbnailStripProxy::reloadItem(int index) {
+    if(thumbnailStrip)
+        thumbnailStrip->reloadItem(index);
+}
+
+void ThumbnailStripProxy::setDirectoryPath(QString path) {
+    Q_UNUSED(path)
+}
+
+void ThumbnailStripProxy::addItem() {
+    if(thumbnailStrip) {
+        thumbnailStrip->addItem();
+    } else {
+        stateBuf.itemCount++;
+    }
+}
+
+void ThumbnailStripProxy::showEvent(QShowEvent *event) {
+    init();
+    QWidget::showEvent(event);
+}
---
+++ qimgv/gui/panels/mainpanel/thumbnailstripproxy.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "gui/panels/mainpanel/thumbnailstrip.h"
+
+struct ThumbnailStripStateBuffer {
+    int selectedIndex = -1;
+    int itemCount;
+};
+
+class ThumbnailStripProxy : public QWidget, public IDirectoryView {
+    Q_OBJECT
+public:
+    ThumbnailStripProxy(QWidget *parent = nullptr);
+    void init();
+
+public slots:
+    virtual void populate(int) Q_DECL_OVERRIDE;
+    virtual void setThumbnail(int pos, std::shared_ptr<Thumbnail> thumb) Q_DECL_OVERRIDE;
+    virtual void selectIndex(int) Q_DECL_OVERRIDE;
+    virtual int  selectedIndex() Q_DECL_OVERRIDE;
+    virtual void focusOn(int) Q_DECL_OVERRIDE;
+    virtual void insertItem(int index) Q_DECL_OVERRIDE;
+    virtual void removeItem(int index) Q_DECL_OVERRIDE;
+    virtual void reloadItem(int index) Q_DECL_OVERRIDE;
+    virtual void setDirectoryPath(QString path) Q_DECL_OVERRIDE;
+    void addItem();
+
+protected:
+    void showEvent(QShowEvent *event) override;
+
+signals:
+    void itemSelected(int) override;
+    void thumbnailsRequested(QList<int>, int, bool, bool);
+
+private:
+    std::shared_ptr<ThumbnailStrip> thumbnailStrip;
+    QVBoxLayout layout;
+    ThumbnailStripStateBuffer stateBuf;
+};
