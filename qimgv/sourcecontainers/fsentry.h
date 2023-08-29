6beccb0fd3ff70b6bb1883f0d34cd28f858d7ab2
周上行Ryer <melaiiryer@gmail.com>
2023-08-29 12:14:54 +0800

    fix: file/dir name codec error (#354)
    current method to resolve path is not corret due to encoding issues,
    simply using QDir/QFileInfo to replace std::filesystem and std::string.

--- qimgv/sourcecontainers/fsentry.h
+++ qimgv/sourcecontainers/fsentry.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <QString>
+#include <QDateTime>
 #include <filesystem>
 #include "utils/stuff.h"
 
@@ -7,13 +8,13 @@ class FSEntry {
 public:
     FSEntry();
     FSEntry( const QString &filePath);
-    FSEntry( QString _path, QString _name, std::uintmax_t _size, std::filesystem::file_time_type _modifyTime, bool _isDirectory);
+    FSEntry( QString _path, QString _name, std::uintmax_t _size, QDateTime _modifyTime, bool _isDirectory);
     FSEntry( QString _path, QString _name, std::uintmax_t _size, bool _isDirectory);
     FSEntry( QString _path, QString _name, bool _isDirectory);
     bool operator==(const QString &anotherPath) const;
 
     QString path, name;
     std::uintmax_t size;
-    std::filesystem::file_time_type modifyTime;
+    QDateTime modifyTime;
     bool isDirectory;
 };
